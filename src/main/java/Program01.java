87108438954b53c333c14e51d33bebaff5e948d8
koory1st <32436334@qq.com>
2019-01-11 23:06:18 +0800

    修改使用slf4j的log

--- src/main/java/Program01.java
+++ src/main/java/Program01.java
@@ -8,40 +8,42 @@ import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.Observer;
 import io.reactivex.disposables.Disposable;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class Program01 {
     public static void main(String[] args) {
         Observable<Integer> observable1 = Observable.create(new ObservableOnSubscribe<Integer>() {
             @Override
             public void subscribe(ObservableEmitter<Integer> observableEmitter) throws Exception {
                 observableEmitter.onNext(1);
-                System.out.println("emmit:1");
+                log.info("emmit:1");
                 observableEmitter.onNext(2);
-                System.out.println("emmit:2");
+                log.info("emmit:2");
                 observableEmitter.onNext(3);
-                System.out.println("emmit:3");
+                log.info("emmit:3");
             }
         });
 
         Observer<Integer> observer = new Observer<Integer>() {
             @Override
             public void onSubscribe(Disposable disposable) {
-                System.out.println("onSubscribe");
+                log.info("onSubscribe");
             }
 
             @Override
             public void onNext(Integer integer) {
-                System.out.println("onNext:" + integer);
+                log.info("onNext:" + integer);
             }
 
             @Override
             public void onError(Throwable throwable) {
-                System.out.println("onError");
+                log.info("onError");
             }
 
             @Override
             public void onComplete() {
-                System.out.println("onComplete");
+                log.info("onComplete");
             }
         };
 
